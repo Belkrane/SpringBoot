f329209bb2cfb60101494866721dca398a680ecb
Belkrane <jonghagi77@gmail.com>
2022-09-21 08:20:57 +0900

    Use SpringBoot Containor

--- src/main/java/hello/core/OrderApp.java
+++ src/main/java/hello/core/OrderApp.java
@@ -7,20 +7,22 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.order.Order;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class OrderApp {
     public static void main(String[] args) {
         Long memberId = 1L;
         Member member = new Member(memberId, "memberA", Grade.VIP);
 
-        AppConfig appConfig = new AppConfig();
+        //AppConfig appConfig = new AppConfig();
+        ApplicationContext ApplicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
 
-        MemberService memberService = appConfig.memberService();
+        MemberService memberService = ApplicationContext.getBean("memberService", MemberService.class);
+        OrderService orderService = ApplicationContext.getBean("orderService", OrderService.class);
 
         memberService.join(member);
 
-        OrderService orderService = appConfig.orderService();
-        
         Order order = orderService.createOrder(memberId, "ItemA", 10000);
 
         System.out.println("order = " + order);
