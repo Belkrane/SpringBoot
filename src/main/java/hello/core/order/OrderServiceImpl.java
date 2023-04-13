ed22604a83bf71482dd164fd0aab45c1bff8fc22
Belkrane <jonghagi77@gmail.com>
2023-04-13 13:15:14 +0900

    Autowired duplication

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -3,24 +3,25 @@ package hello.core.order;
 import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class OrderServiceImpl implements OrderService {
-    private final DiscountPolicy discountPolicy;
+    private final DiscountPolicy rateDiscountPolicy;
     private final MemberRepository memberRepository;
 
     @Autowired
-    public OrderServiceImpl(DiscountPolicy discountPolicy, MemberRepository memberRepository) {
-        this.discountPolicy = discountPolicy;
+    public OrderServiceImpl(DiscountPolicy rateDiscountPolicy, MemberRepository memberRepository) {
+        this.rateDiscountPolicy = rateDiscountPolicy;
         this.memberRepository = memberRepository;
     }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
         Member member = memberRepository.findById(memberId);
-        int discountPrice = discountPolicy.discount(member, itemPrice);
+        int discountPrice = rateDiscountPolicy.discount(member, itemPrice);
 
 
         return new Order(memberId, itemName, itemPrice, discountPrice);
